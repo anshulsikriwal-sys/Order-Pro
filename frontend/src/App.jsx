e8852ec55bdb2c0f7747ef6dd4e02bd0b4797ef4
anshulsikriwal-sys <anshulsikriwal@gmail.com>
2026-07-30 06:47:02 +0530

    Test health route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/common/Navbar";
+import Footer from "./components/common/Footer";
+
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
-function App(){
 
-return (
-<BrowserRouter>
+function App() {
+  return (
+    <BrowserRouter>
 
-<Routes>
+      <Navbar />
 
-<Route path="/" element={<Home/>}/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+      </Routes>
 
-<Route path="/menu" element={<Menu/>}/>
+      <Footer />
 
-</Routes>
-
-</BrowserRouter>
-)
-
+    </BrowserRouter>
+  );
 }
 
 export default App;
